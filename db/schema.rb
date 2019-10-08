e31d4addd106f900ccfac0f2e88c99c08f8e4511
progbmy <skuter63@mail.ru>
2019-10-09 02:24:51 +0400

    Migrate User + Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_213912) do
+ActiveRecord::Schema.define(version: 2019_10_08_222107) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "manufacturer"
+    t.string "name"
+    t.integer "vendor_code"
+    t.string "weight"
+    t.string "price"
+    t.string "minimal_amount"
+    t.string "title"
+    t.string "h1"
+    t.string "key_words"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "surname"
